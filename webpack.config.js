195b57567908683b1e0b10ffb0399baa1b1ed7bb
yago <yago.zhvaniya@gmail.com>
2022-12-06 11:11:18 +0300

    первый коммит

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/App.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './build'),
+  },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css?$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
